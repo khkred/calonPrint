cdf3cf10d9ad935f8ce4ec24bd08fdbeeb6d0a40
khkr <32921476+khkred@users.noreply.github.com>
2022-04-23 13:30:13 +0530

    Added Constants file to store URLS and other values.

---
+++ app/src/main/java/sg/com/argus/www/conquestgroup/utils/Constants.java
@@ -0,0 +1,6 @@
+package sg.com.argus.www.conquestgroup.utils;
+
+public class Constants {
+    public static final String  BASE_URL = "http://www.train.enam.gov.in";
+    public static final String GET_STATES_URL = "https://train.enam.gov.in/NamWebSrv/rest/MastersUpdate/getStates";
+}
