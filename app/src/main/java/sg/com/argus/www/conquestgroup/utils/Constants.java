29211c9870af2e3f64ddf4882ab41995410db872
khkr <32921476+khkred@users.noreply.github.com>
2022-05-01 12:27:45 +0530

    Added Url for summary print

--- app/src/main/java/sg/com/argus/www/conquestgroup/utils/Constants.java
+++ app/src/main/java/sg/com/argus/www/conquestgroup/utils/Constants.java
@@ -1,11 +1,12 @@
 package sg.com.argus.www.conquestgroup.utils;
 
 public class Constants {
-    public static final String  BASE_URL = "https://www.enam.gov.in/NamWebSrv/rest/";
+    public static final String  BASE_URL = "http://www.train.enam.gov.in/NamWebSrv/rest/";
 
     public static final String GET_STATES_URL = BASE_URL + "MastersUpdate/getStates";
     public static final String VERIFY_USER_URL =  BASE_URL + "verifyUser";
     public static final String SEND_BAG_WEIGHT_URL = BASE_URL + "SendBagWeightAuto";
+    public static final String SUMMARY_PRINT_URL = BASE_URL+ "getPosWeighmentInfo";
 
     //WelcomeUserActivity
     public static final String GET_FEE_CATEGORY_URL = BASE_URL + "FeeCategory/getFeeCategoryListAutoSaleAgreement";
