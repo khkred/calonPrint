76423a29591803c222a9f7c3262e2c77e3d2bd6c
khkr <32921476+khkred@users.noreply.github.com>
2022-05-01 22:48:02 +0530

    Initial SummaryActivity added

---
+++ app/src/main/java/sg/com/argus/www/conquestgroup/activities/SummaryActivity.java
@@ -0,0 +1,16 @@
+package sg.com.argus.www.conquestgroup.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import sg.com.argus.www.conquestgroup.R;
+
+public class SummaryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acitivity_summary);
+
+    }
+}
