f2cc301ea034254dcd515ab41c9c7d3aa25aba75
khkr <32921476+khkred@users.noreply.github.com>
2022-05-09 12:01:28 +0530

    Added TableItem Model Class to print tables

---
+++ app/src/main/java/sg/com/argus/www/conquestgroup/models/TableItem.java
@@ -0,0 +1,37 @@
+package sg.com.argus.www.conquestgroup.models;
+
+public class TableItem {
+    private String[] text;
+    private int[] width;
+    private int[] align;
+
+    public TableItem() {
+        text = new String[]{"test","test"};
+        width = new int[]{1,1};
+        align = new int[]{0,1};
+    }
+
+    public String[] getText() {
+        return text;
+    }
+
+    public void setText(String[] text) {
+        this.text = text;
+    }
+
+    public int[] getWidth() {
+        return width;
+    }
+
+    public void setWidth(int[] width) {
+        this.width = width;
+    }
+
+    public int[] getAlign() {
+        return align;
+    }
+
+    public void setAlign(int[] align) {
+        this.align = align;
+    }
+}
